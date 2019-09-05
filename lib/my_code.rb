b6d12db6b3dff3e917a9e87b2898308ba352ae1a
sunny-utility-4827 <mzakzook@me.com>
2019-09-05 21:23:53 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -25,7 +25,7 @@ def map_to_square(source_array)
 end
 
 def reduce_to_total(source_array, starting_point = 0)
-  total = 0
+  total = starting_point
   source_array.length.times do |index|
     total += source_array[index]
   end
