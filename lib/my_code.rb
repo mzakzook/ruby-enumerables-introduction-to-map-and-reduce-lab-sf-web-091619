f1117bce824ced3614dc2eb775db1ca773b272f3
sunny-utility-4827 <mzakzook@me.com>
2019-09-05 21:30:12 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -35,7 +35,7 @@ end
 def reduce_to_all_true(source_array)
   check = true
   source_array.length.times do |index|
-    if source_array[index] == false 
+    if source_array[index] != true 
       return false
     end
   check
