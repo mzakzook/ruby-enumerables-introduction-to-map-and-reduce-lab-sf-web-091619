f1da7a0245d05142bd879f0f65fd699158d5a2b3
sunny-utility-4827 <mzakzook@me.com>
2019-09-05 21:10:23 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -16,3 +16,10 @@ def map_to_double(source_array)
   end
   source_array
 end
+
+def map_to_square(source_array)
+  source_array.length.times do |index|
+    source_array[index] **= 2
+  end
+  source_array
+end
